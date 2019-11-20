adb99af8d605a2f7add6918d4dc58fcd994a7ad4
Nurvedin Dizdarevic <nurvedin@hotmail.com>
2019-11-20 08:29:53 +0100

    Getting my package.json in order and setting things up with help from the demo on coursepress

---
+++ app.js
@@ -0,0 +1,6 @@
+const funcModule = require('./lib/scraper.js')
+console.log(funcModule.sayHello())
+
+// const Shouter = require('./lib/Shouter.js')
+// const shouter = new Shouter()
+// console.log(shouter.sayHello())
