95d7a73fdcd9d2229e0c446de40712e309347d54
Nurvedin Dizdarevic <nurvedin@hotmail.com>
2019-11-21 10:45:34 +0100

    Getting the days from every person

--- app.js
+++ app.js
@@ -1,6 +1,85 @@
-const funcModule = require('./lib/scraper.js')
-console.log(funcModule.sayHello())
+// import getWebsite from './lib/Scraper.js'
 
-// const Shouter = require('./lib/Shouter.js')
-// const shouter = new Shouter()
-// console.log(shouter.sayHello())
+// getWebsite()
+// const request = require('./lib/Scraper.js')
+// console.log(request)
+// request('http://vhost3.lnu.se:20080/weekend', (error, response, html) => {
+//  if (!error && response.statusCode === 200) {
+//    console.log(html)
+//  }
+// })
+
+// const funcModule = require('./lib/Scraper.js')
+// console.log(funcModule)
+
+// const Scraper = require('./lib/Scraper.js')
+// const scraper = require('./lib/Scraper.js')
+// console.log(scraper)
+// const request = require('./lib/Scraper.js')
+// console.log(request)
+// request.then(function (result) {
+//  console.log(result)
+// })
+// const url = 'http://vhost3.lnu.se:20080/weekend'
+
+// Promise((resolve, reject) => {
+//  request(url, (error, response, body) => {
+//    if (error) {
+//      reject(error)
+//    }
+//    else if (response.statusCode === 200) {
+//      resolve(body)
+//      console.log(body)
+//    }
+//  })
+// })
+/**
+ *
+ */
+const request = require('request')
+const cheerio = require('cheerio')
+request('http://vhost3.lnu.se:20080/weekend', (error, response, html) => {
+  if (!error && response.statusCode === 200) {
+    const $ = cheerio.load(html)
+    // console.log($)
+    const calendar = $('a')
+    // console.log(calendar.attr('href'))
+    const link = calendar.attr('href')
+    // console.log(link)
+
+    request(link, (error, response, html) => {
+      if (!error && response.statusCode === 200) {
+        const $ = cheerio.load(html)
+        const calendarLinks = $('a')
+        calendarLinks.each(function () {
+          // const text = $(this).text()
+          const links = $(this).attr('href')
+          // console.log(text + ' ===========>>>> ' + links)
+
+          request(link + links, (error, response, html) => {
+            if (!error && response.statusCode === 200) {
+              const $ = cheerio.load(html)
+              // console.log($(this).text())
+              // console.log($.html())
+              const days = $('th')
+              // console.log(days.text())
+              // console.log(days.html())
+              // const freeDays = $('td')
+              days.each((i, elem) => {
+                const temp = $(elem).text()
+                console.log(temp)
+                // const temp = days.text()
+                // console.log(temp.split('day', 3))
+              })
+              // freeDays.each(function () {
+              // console.log($(this).text())
+              // })
+              // console.log(days)
+              // console.log(freeDays)
+            }
+          })
+        })
+      }
+    })
+  }
+})
