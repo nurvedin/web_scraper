fe0075c46fe1b7c9b04d6673eeb106f378e9be31
Nurvedin Dizdarevic <nurvedin@hotmail.com>
2019-11-25 10:56:45 +0100

    Getting the available days from every calendar

--- app.js
+++ app.js
@@ -1,81 +1,45 @@
-// import getWebsite from './lib/Scraper.js'
-
-// getWebsite()
-// const request = require('./lib/Scraper.js')
-// console.log(request)
-// request('http://vhost3.lnu.se:20080/weekend', (error, response, html) => {
-//  if (!error && response.statusCode === 200) {
-//    console.log(html)
-//  }
-// })
-
-// const funcModule = require('./lib/Scraper.js')
-// console.log(funcModule)
-
-// const Scraper = require('./lib/Scraper.js')
-// const scraper = require('./lib/Scraper.js')
-// console.log(scraper)
-// const request = require('./lib/Scraper.js')
-// console.log(request)
-// request.then(function (result) {
-//  console.log(result)
-// })
-// const url = 'http://vhost3.lnu.se:20080/weekend'
-
-// Promise((resolve, reject) => {
-//  request(url, (error, response, body) => {
-//    if (error) {
-//      reject(error)
-//    }
-//    else if (response.statusCode === 200) {
-//      resolve(body)
-//      console.log(body)
-//    }
-//  })
-// })
-/**
- *
- */
 const request = require('request')
 const cheerio = require('cheerio')
-request('http://vhost3.lnu.se:20080/weekend', (error, response, html) => {
+const rootLink = 'http://vhost3.lnu.se:20080/weekend'
+request(rootLink, (error, response, html) => {
   if (!error && response.statusCode === 200) {
     const $ = cheerio.load(html)
-    // console.log($)
     const calendar = $('a')
-    // console.log(calendar.attr('href'))
     const link = calendar.attr('href')
-    // console.log(link)
 
     request(link, (error, response, html) => {
       if (!error && response.statusCode === 200) {
         const $ = cheerio.load(html)
         const calendarLinks = $('a')
         calendarLinks.each(function () {
-          // const text = $(this).text()
-          const links = $(this).attr('href')
-          // console.log(text + ' ===========>>>> ' + links)
+          const allLinks = $(this).attr('href')
 
-          request(link + links, (error, response, html) => {
+          request(link + allLinks, (error, response, html) => {
             if (!error && response.statusCode === 200) {
+              let counter = 0
               const $ = cheerio.load(html)
-              // console.log($(this).text())
-              // console.log($.html())
-              const days = $('th')
-              // console.log(days.text())
-              // console.log(days.html())
-              // const freeDays = $('td')
-              days.each((i, elem) => {
-                const temp = $(elem).text()
-                console.log(temp)
-                // const temp = days.text()
-                // console.log(temp.split('day', 3))
-              })
-              // freeDays.each(function () {
-              // console.log($(this).text())
-              // })
-              // console.log(days)
-              // console.log(freeDays)
+              const weekendDays = $('th').text()
+              const availableDays = $('td').text()
+              const dayArr = [weekendDays.split('day', 3)]
+              const availableArr = [availableDays.match(/.{1,2}/g)]
+              // console.log(dayArr[0][0])
+              // console.log(availableArr[0][0])
+              if (dayArr[0][0] === 'Fri' && availableArr[0][0] === 'ok') {
+                counter++
+              } else if (dayArr[0][0] === 'Fri' && availableArr[0][0] === 'OK') {
+                counter++
+              } else if (dayArr[1][1] === 'Satur' && availableArr[1][1] === 'ok') {
+                counter++
+              } else if (dayArr[1][1] === 'Satur' && availableArr[1][1] === 'OK') {
+                counter++
+              } else if (dayArr[2][2] === 'Sun' && availableArr[2][2] === 'ok') {
+                counter++
+              } else if (dayArr[2][2] === 'Sun' && availableArr[2][2] === 'OK') {
+                counter++
+              }
+              if (counter === 1) {
+                console.log('Friday works for everybody')
+              }
             }
           })
         })
