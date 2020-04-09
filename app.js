cd60570af76a3e22610152c987bc3f04bb8944ba
nurvedin <nurvedin@hotmail.com>
2020-04-09 08:43:07 +0200

    successfully logged in into zekes bar

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const request = require('request')
-const cheerio = require('cheerio')
+// const request = require('request')
+// const cheerio = require('cheerio')
 // const links = require('./lib/links.js')
 // const rp = require('request-promise')
 
-const link = 'http://vhost3.lnu.se:20080/weekend'
+// const link = 'http://vhost3.lnu.se:20080/weekend'
 const scrape = require('./lib/calendarLinks.js')
 const movieSearch = require('./lib/movie.js')
+const dinnerSearch = require('./lib/dinner.js')
 
 scrape.availableDays().then(function (link) {
   process.stdout.write('Scraping links')
@@ -19,146 +20,26 @@ scrape.availableDays().then(function (link) {
   process.stdout.write('.')
 }).then(function () {
   process.stdout.write('OK!')
-}).catch(function (err) {
-  console.log(err)
-})
 
-movieSearch.cinemaLink().then(function () {
-  process.stdout.write('Scraping showtimes')
-}).then(function () {
-  process.stdout.write('.')
-}).then(function () {
-  process.stdout.write('.')
-  console.log('\n')
-}).catch(function (err) {
-  console.log(err)
-})
-/*
-request(rootLink, (error, response, html) => {
-  if (!error && response.statusCode === 200) {
-    const $ = cheerio.load(html)
-    const calendar = $('a')
-    const link = calendar.attr('href')
-*/
-/*
-const link = async (links) => {
-  console.log('Scraping links......')
-  await links()
-  console.log('OK!')
-}
-*/
-/*
-request(link, (error, response, html) => {
-  if (!error && response.statusCode === 200) {
-    const $ = cheerio.load(html)
-    const calendarLinks = $('a')
-    calendarLinks.each(function () {
-      const allLinks = $(this).attr('href')
-
-      request(link + allLinks, (error, response, html) => {
-        if (!error && response.statusCode === 200) {
-          let counter = 0
-          const freeDay = []
-          const $ = cheerio.load(html)
-          const weekendDays = $('th').text()
-          const availableDays = $('td').text()
-          const dayArr = [weekendDays.split('day', 3)]
-          const availableArr = [availableDays.match(/.{1,2}/g)]
-
-          if (dayArr[0][0] === 'Fri' && availableArr[0][0] === 'ok') {
-            counter++
-            freeDay.push(dayArr[0][0])
-          } else if (dayArr[0][0] === 'Fri' && availableArr[0][0] === 'OK') {
-            counter++
-            freeDay.push(dayArr[0][0])
-          } else if (dayArr[1][1] === 'Satur' && availableArr[1][1] === 'ok') {
-            counter++
-            freeDay.push(dayArr[1][1])
-          } else if (dayArr[1][1] === 'Satur' && availableArr[1][1] === 'OK') {
-            counter++
-            freeDay.push(dayArr[1][1])
-          } else if (dayArr[2][2] === 'Sun' && availableArr[2][2] === 'ok') {
-            counter++
-            freeDay.push(dayArr[2][2])
-          } else if (dayArr[2][2] === 'Sun' && availableArr[2][2] === 'OK') {
-            counter++
-            freeDay.push(dayArr[2][2])
-          } else {
-            console.log('There is no day that works for everybody')
-          }
-          if (counter === 1) {
-            // console.log('Friday')
-          }
-        }
-      })
-    })
-  }
-})
-*/
-
-request(link, (error, response, html) => {
-  const linkArr = []
-  if (!error && response.statusCode === 200) {
-    const $ = cheerio.load(html)
-    const links = $('a')
-    links.each(function () {
-      const allLinks = $(this).attr('href')
-      linkArr.push(allLinks)
-    })
-  }
-  const dinnerUrl = linkArr[2] + '/'
-
-  const jsonData = {
-    username: 'zeke',
-    password: 'coys',
-    submit: 'login'
-  }
-
-  const dinnerTimes = []
-  request.post({
-    url: dinnerUrl + 'login',
-    json: jsonData
-  }, function (error, response, body) {
-    if (!error && response.statusCode === 302) {
-      const cookie = response.headers['set-cookie']
-      request.post({
-        url: dinnerUrl + 'login',
-        json: jsonData
-      }, function (error, response, body) {
-        if (!error && response.statusCode === 302) {
-          const location = response.headers.location
-          request({
-            url: dinnerUrl + location,
-            headers: {
-              cookie: cookie
-            }
-          }, function (error, response, html) {
-            if (!error && response.statusCode === 200) {
-              const $ = cheerio.load(html)
-              const friday = $('.WordSection2').text()
-              dinnerTimes.push(friday.trim().split('\n\n'))
-              // console.log(dinnerTimes)
-              // const saturday = $('.WordSection4').text()
-              // console.log(saturday)
-              // const sunday = $('.WordSection6').text()
-              // console.log(sunday)
-              // const friday = allDays.split('day', 3)
-              // console.log(friday)
-              // if ('dagen som alla kan' === 'Friday') {
-              //  console.log(friday)
-              // } else if ('dagen som alla kan' === 'Saturday') {
-              //  console.log(saturday)
-              // } else if ('dagen alla är lediga' === 'Sunday') {
-              //  console.log(sunday)
-              // }
-            }
-          })
-        } else {
-          return error
-        }
-      })
-    } else {
-      return error
-    }
+  movieSearch.cinemaLink().then(function () {
+    process.stdout.write('Scraping showtimes')
+  }).then(function () {
+    process.stdout.write('.')
+  }).then(function () {
+    process.stdout.write('.')
+    console.log('\n')
+  }).catch(function (err) {
+    console.log(err)
   })
+  
+}).catch(function (err) {
+  console.log(err)
+})
+
+
+
+dinnerSearch.dinnerLink().then(function () {
+  process.stdout.write('Scraping dinnertimes')
+}).catch(function (err) {
+  console.log(err)
 })
